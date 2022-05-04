b40b1f262f6779844ecf93ca646c4276090f0b36
Code_xxmyyds <xxmyyds@163.com>
2022-05-04 21:24:34 +0800

    easy-exclude

---
+++ src/43-easy-exclude/template.ts
@@ -0,0 +1,19 @@
+type MyExclude<T, U> = T extends U ? never : T
+
+// js
+function MyExclude(T, U) {
+  const res = []
+  for (let i = 0; i < T.length; i++) {
+    const t = T[i]
+    let boo = false
+    for (let j = 0; j < U.length; j++) {
+      const u = U[j]
+      if (t === u) {
+        boo = true
+      }
+    }
+    if (!boo) {
+      res.push(t)
+    }
+  }
+}
